90b85eafeec3b1ea6c1d17d2f69e79b4890251ac
CJ Faraon <faraonc@outlook.com>
2017-11-25 17:47:54 -0800

    Get ready for the project.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -67,6 +67,14 @@ public class MainActivity extends AppCompatActivity {
      */
     private void displayPrice(double number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        priceTextView.setText("Total: " + NumberFormat.getCurrencyInstance().format(number) + "\nThank you!");
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
