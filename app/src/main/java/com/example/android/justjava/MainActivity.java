3a74b7e162ae64bad2e8e937a07c0539a1afbcc2
faraonc <faraonc@outlook.com>
2017-11-28 21:43:58 -0800

    Finish project 3

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,19 +1,13 @@
 package com.example.android.justjava;
 
-/**
- * IMPORTANT: Add your package below. Package name can be found in the project's AndroidManifest.xml file.
- * This is the package name our example uses:
- * <p>
- * package com.example.android.justjava;
- */
-
+import java.text.NumberFormat;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
-
-import java.text.NumberFormat;
+import android.widget.EditText;
 
 /**
  * This app displays an order form to order coffee.
@@ -21,27 +15,32 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
 
     private int quantity = 0;
-    private final static double COFFEE_PRICE = 4.25;
+    private EditText nameText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.nameText = ((EditText) findViewById(R.id.customer_edit_text_view));
     }
 
+    /**
+     * This method is called when the plus button is clicked.
+     */
     public void increment(View view) {
-        if (this.quantity == Integer.MAX_VALUE) {
-            this.quantity = 0;
+        if(quantity != Integer.MAX_VALUE) {
+            quantity = quantity + 1;
+            displayQuantity(quantity);
         }
-        displayQuantity(++this.quantity);
     }
 
+    /**
+     * This method is called when the minus button is clicked.
+     */
     public void decrement(View view) {
-        if (this.quantity == 0) {
-            this.quantity = 0;
-            displayQuantity(this.quantity);
-        } else {
-            displayQuantity(--this.quantity);
+        if(quantity - 1 >= 0) {
+            quantity = quantity - 1;
+            displayQuantity(quantity);
         }
     }
 
@@ -49,46 +48,68 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        if (this.quantity * COFFEE_PRICE < Double.MAX_VALUE) {
-            displayPrice(calculatePrice());
+        // Figure out if the user wants whipped cream topping
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+        // Figure out if the user wants whipped cream topping
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+
+        // Calculate the price
+        double price = calculatePrice();
+
+        // Display the order summary on the screen
+        if (price > 0) {
+            String message = createOrderSummary(price, hasWhippedCream, hasChocolate);
+            displayMessage(message);
+        } else {
+            displayMessage(NumberFormat.getCurrencyInstance().format(0));
         }
     }
 
     /**
-     * Calculates the price of the order based on the current quantity.
+     * Calculates the price of the order.
      *
-     * @return the price
+     * @return total price
      */
     private double calculatePrice() {
-        return this.quantity * COFFEE_PRICE;
+        return quantity * 5;
+    }
+
+    /**
+     * Create summary of the order.
+     *
+     * @param price           of the order
+     * @param addWhippedCream is whether or not to add whipped cream to the coffee
+     * @param addChocolate    is whether or not to add chocolate to the coffee
+     * @return text summary
+     */
+    private String createOrderSummary(double price, boolean addWhippedCream, boolean addChocolate) {
+        String priceMessage = "Name: " + this.nameText.getText().toString();
+        priceMessage += "\nAdd whipped cream? " + addWhippedCream;
+        priceMessage += "\nAdd chocolate? " + addChocolate;
+        priceMessage += "\nQuantity: " + quantity;
+        priceMessage += "\nTotal: " + NumberFormat.getCurrencyInstance().format(price);
+        priceMessage += "\nThank you!";
+        return priceMessage;
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void displayQuantity(int numberOfCoffee) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(String.valueOf(numberOfCoffee));
+    private void displayQuantity(int numberOfCoffees) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + numberOfCoffees);
     }
 
     /**
-     * This method displays the given price on the screen.
+     * This method displays the given text on the screen.
      */
-    private void displayPrice(double price) {
+    private void displayMessage(String message) {
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        if (price > 0) {
-            orderSummaryTextView.setText(createOrderSummary(price));
-        } else {
-            orderSummaryTextView.setText(NumberFormat.getCurrencyInstance().format(0));
-        }
-    }
-
-    private String createOrderSummary(double price){
-        String customer = "Kaptain Kunal";
-        return "Name: " + customer +
-                "\nQuantity: " + this.quantity +
-                "\nTotal: " + NumberFormat.getCurrencyInstance().format(price) +
-                "\nThank you!";
+        orderSummaryTextView.setText(message);
     }
 
 }
