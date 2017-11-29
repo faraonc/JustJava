5523a6780f6693fa1a7fbe565fdd77851d4083b4
faraonc <faraonc@outlook.com>
2017-11-29 15:09:42 -0800

    Just Java app for Udacity module 3.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
+import android.net.Uri;
 
 /**
  * This app displays an order form to order coffee.
@@ -16,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
 
     private int quantity = 0;
     private EditText nameText;
+    private boolean addWhippedCream = false;
+    private boolean addChocolate = false;
+
+    private final static double BASE_PRICE = 4.25;
+    private final static double WHIPPED_CREAM_PRICE = 1.0;
+    private final static double CHOCOLATE_PRICE = 4.25;
+    private final static int MAX_QUANTITY = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +38,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
-        if(quantity != Integer.MAX_VALUE) {
-            quantity = quantity + 1;
-            displayQuantity(quantity);
+        if (this.quantity != this.MAX_QUANTITY) {
+            displayQuantity(++this.quantity);
+        }else{
+            Toast.makeText(this, this.getString(R.string.over), Toast.LENGTH_SHORT ).show();
         }
     }
 
@@ -38,9 +49,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
-        if(quantity - 1 >= 0) {
-            quantity = quantity - 1;
-            displayQuantity(quantity);
+        if (this.quantity - 1 >= 0) {
+            displayQuantity(--this.quantity);
+        }else{
+            Toast.makeText(this, this.getString(R.string.negative), Toast.LENGTH_SHORT ).show();
         }
     }
 
@@ -50,21 +62,19 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view) {
         // Figure out if the user wants whipped cream topping
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
-        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        this.addWhippedCream = whippedCreamCheckBox.isChecked();
 
         // Figure out if the user wants whipped cream topping
         CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
-        boolean hasChocolate = chocolateCheckBox.isChecked();
+        this.addChocolate = chocolateCheckBox.isChecked();
 
         // Calculate the price
         double price = calculatePrice();
 
         // Display the order summary on the screen
         if (price > 0) {
-            String message = createOrderSummary(price, hasWhippedCream, hasChocolate);
+            String message = createOrderSummary(price, addWhippedCream, addChocolate);
             displayMessage(message);
-        } else {
-            displayMessage(NumberFormat.getCurrencyInstance().format(0));
         }
     }
 
@@ -74,7 +84,14 @@ public class MainActivity extends AppCompatActivity {
      * @return total price
      */
     private double calculatePrice() {
-        return quantity * 5;
+        double total = this.BASE_PRICE;
+        if(this.addWhippedCream){
+            total += this.WHIPPED_CREAM_PRICE;
+        }
+        if(this.addChocolate){
+            total += this.CHOCOLATE_PRICE;
+        }
+        return quantity * total;
     }
 
     /**
@@ -86,12 +103,12 @@ public class MainActivity extends AppCompatActivity {
      * @return text summary
      */
     private String createOrderSummary(double price, boolean addWhippedCream, boolean addChocolate) {
-        String priceMessage = "Name: " + this.nameText.getText().toString();
-        priceMessage += "\nAdd whipped cream? " + addWhippedCream;
-        priceMessage += "\nAdd chocolate? " + addChocolate;
-        priceMessage += "\nQuantity: " + quantity;
-        priceMessage += "\nTotal: " + NumberFormat.getCurrencyInstance().format(price);
-        priceMessage += "\nThank you!";
+        String priceMessage = this.getString(R.string.order_summary_name) + this.nameText.getText().toString();
+        priceMessage += "\n" + this.getString(R.string.order_summary_whipped_cream) + addWhippedCream;
+        priceMessage += "\n" + this.getString(R.string.order_summary_chocolate) + addChocolate;
+        priceMessage += "\n" + this.getString(R.string.order_summary_quantity) + quantity;
+        priceMessage += "\n" + this.getString(R.string.order_summary_price) + NumberFormat.getCurrencyInstance().format(price);
+        priceMessage += "\n" + this.getString(R.string.thank_you);
         return priceMessage;
     }
 
@@ -99,17 +116,21 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given quantity value on the screen.
      */
     private void displayQuantity(int numberOfCoffees) {
-        TextView quantityTextView = (TextView) findViewById(
-                R.id.quantity_text_view);
-        quantityTextView.setText("" + numberOfCoffees);
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(String.valueOf(numberOfCoffees));
     }
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, this.getString(R.string.order_summary_email_subject));
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
 }
